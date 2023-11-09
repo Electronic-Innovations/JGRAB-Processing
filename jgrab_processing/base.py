7db6e74afdc2be3653827e0a54aeb5e50a7caa2b
Daniel Pink <dr.pink@gmail.com>
2023-11-09 14:08:21 +1000

    Removing most of the template as it is too difficult for me to follow in one hit

--- jgrab_processing/base.py
+++ jgrab_processing/base.py
@@ -1,17 +1,138 @@
-"""
-jgrab_processing base module.
+import sys
+import os
+from os import listdir
+from os.path import isfile, join, splitext
+import matplotlib.pyplot as plt
+import math
+from scipy import optimize
+import numpy as np
 
-This is the principal module of the jgrab_processing project.
-here you put your main classes and objects.
+def rms(numbers):
+    # Check for an empty list to avoid division by zero
+    if len(numbers) == 0:
+        return None
 
-Be creative! do whatever you want!
+    # Calculate the sum of the squares of the numbers
+    sum_of_squares = sum(x ** 2 for x in numbers)
 
-If you want to replace this with a Flask application run:
+    # Calculate the mean of the squares
+    mean_of_squares = sum_of_squares / len(numbers)
 
-    $ make init
+    # Take the square root to get the RMS
+    rms = math.sqrt(mean_of_squares)
 
-and then choose `flask` as template.
-"""
+    return rms
 
-# example constant variable
-NAME = "jgrab_processing"
+def formatNumbers(numbers):
+    return ["{:.2f}".format(number) for number in numbers]
+
+def sin_wave(x, amplitude, frequency, phase):
+    return amplitude * np.sin((frequency * x)+phase)
+
+def THD_N(x_values, y_values):
+    params, params_covariance = optimize.curve_fit(sin_wave, x_values, y_values, p0=[12000, 0.1, 1])
+    fundamental = sin_wave(x_values, params[0], params[1], params[2])
+    noise = y_values - fundamental
+    return (rms(noise) / rms(fundamental) * 100)
+
+
+
+
+if __name__ == "__main__":
+    arg = sys.argv[1]
+    # print(arg)
+
+    start_reading_data = False
+    data = [[],[],[],[],[]]
+    timestamp = None
+    col_index = 0
+
+    with open(arg) as f:
+        for line in f:
+            if start_reading_data:
+                if line.startswith("%%"):
+                    col_index = col_index + 1
+                else:
+                    data[col_index].append(int(line.split()[0]))
+            if line.startswith("JGRAB"):
+                start_reading_data = True
+
+    # print(data)
+
+    # Create x-axis values (e.g., for the time points)
+    x_values = range(len(data[0]))  # Assuming the x-axis represents time or index
+
+    labels = ['Dc-V','Sph-Unscaled','RphV','Rphl-Unscaled','SphV']
+
+    max_values = [max(values) for values in data]
+    min_values = [min(values) for values in data]
+    rms_values = [rms(values) for values in data]
+    # print(max_values)
+    # print(min_values)
+    # print(formatNumbers(rms_values))
+
+    # Attempt to fit a sin wave to each waveform
+
+    parameters = []
+
+    for i in range(len(data)):
+        params, params_covariance = optimize.curve_fit(sin_wave, x_values, data[i], p0=[12000, 0.1, 1])
+        parameters.append(params)
+
+    # print(parameters)
+
+    
+    THD_Values = [THD_N(x_values, data[0]), THD_N(x_values, data[1]), THD_N(x_values, data[2]), THD_N(x_values, data[3]), THD_N(x_values, data[4])]
+    # print(THD_N(x_values, data[0]))
+    # print(THD_N(x_values, data[1]))
+    # print(THD_N(x_values, data[2]))
+    # print(THD_N(x_values, data[3]))
+    # print(THD_N(x_values, data[4]))
+
+
+    # Plot each column of data
+    #for i in range(len(data)):
+    #    plt.plot(x_values, data[i], label=f'Column {i + 1}')
+    # print(60 * params[2])
+    # print([(x + (60 * params[2])) for x in x_values])
+
+    params, params_covariance = optimize.curve_fit(sin_wave, x_values, data[2], p0=[12000, 0.1, 1])
+    print(params[2])
+
+    # plt.figure(figsize=(6, 4))
+    # plt.scatter(x_data, y_data, label='Data')
+    plot_x_start = 0
+    plot_x_finish = 240
+    full_x_values = range(plot_x_start, plot_x_finish)
+    phase_angle = 0 if params[0] > 0 else math.pi
+    plt.plot(full_x_values, sin_wave(full_x_values, params[0], params[1], phase_angle),label='Fitted function', color='#D3D3D3')
+        
+
+    # shifted_x_values = [(x + math.copysign((params[2] / params[1]), params[0])) for x in x_values]
+    if params[0] > 0:
+        shifted_x_values = [(x + (params[2] / params[1])) for x in x_values]
+    else:
+        shifted_x_values = [(x + (params[2] / params[1]) + 31) for x in x_values]
+
+    for i in range(len(data)):
+        plt.plot(shifted_x_values, data[i], label=labels[i])
+ 
+    # Customize the plot
+    plt.title(arg)
+    plt.xlabel("Time")
+    plt.ylabel("Voltage")
+    plt.xlim(plot_x_start, plot_x_finish)
+    plt.ylim(-20000, 20000)
+    # plt.legend()
+
+    # Save the plot to a file (e.g., in PNG format)
+    # output_filename = str(int(math.copysign((params[2] / params[1]), params[0]))) + ' ' + os.path.splitext(arg)[0] + ".png"  # Change the filename and format as needed
+    output_filename = os.path.splitext(arg)[0] + ".png"  # Change the filename and format as needed
+    plt.savefig(output_filename, format="png")
+
+    # Display the plot
+    # plt.show()
+
+    print(arg, formatNumbers(THD_Values))
+
+
--- jgrab_processing/cli.py
+++ jgrab_processing/cli.py
@@ -10,19 +10,39 @@ Be creative! do whatever you want!
 
 
 def main():  # pragma: no cover
-    """
-    The main function executes on commands:
-    `python -m jgrab_processing` and `$ jgrab_processing `.
+    parser = argparse.ArgumentParser()
 
-    This is your program's entry point.
+    # -f FORCE 
+    parser.add_argument("folder",nargs="*")
+    parser.add_argument("-f", "--force", action='store_true', help="Process all text files")
 
-    You can change this function to do whatever you want.
-    Examples:
-        * Run a test suite
-        * Run a server
-        * Do some other stuff
-        * Run a command line application (Click, Typer, ArgParse)
-        * List all available tasks
-        * Run an application (Flask, FastAPI, Django, etc.)
-    """
-    print("This will do something")
+    args = parser.parse_args()
+
+    # Define the folder path
+    folder_path = args.folder[0]
+
+    # Define the Python script to run for each file
+    script_to_run = 'parseFile.py'
+
+    # Get a list of all files in the folder with "JGRAB.txt" at the end of the filename
+    data_file_list = [os.path.splitext(f)[0] for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f)) and f.endswith('JGRAB.txt')]
+    chart_file_list = [os.path.splitext(f)[0] for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f)) and f.endswith('.png')]
+
+    # List of .txt files that don't have a matching .png yet
+    unprocessed_list = set([f for f in data_file_list]) ^ set([f for f in chart_file_list])
+
+    if args.force:
+        file_list = data_file_list
+    else:
+        file_list = unprocessed_list
+
+    print("Processing ", len(file_list), " files")
+
+    # Iterate through the list of files and run the script for each file
+    for filename in file_list:
+        # Pass the filename to the script_to_run.py as an argument
+        withExtension = filename + '.txt'
+        script_command = ['python', script_to_run, withExtension]
+
+        # Run the script using subprocess
+        subprocess.run(script_command, cwd=folder_path)
